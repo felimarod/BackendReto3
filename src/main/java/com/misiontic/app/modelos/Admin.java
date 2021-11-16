ee4b2296954bc4826272c28d2e94f947a34078ea
felimarod <afmartinr@correo.udistrital.edu.co>
2021-11-15 22:12:27 -0500

    try 1038

--- src/main/java/com/misiontic/app/modelos/Admin.java
+++ src/main/java/com/misiontic/app/modelos/Admin.java
@@ -5,21 +5,17 @@
  */
 package com.misiontic.app.modelos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+
 import javax.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="puntajes")
+@Table(name="administradores")
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
